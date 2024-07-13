5c9bc7c99c08ef1065b91bccfd0a0c94add8a5e2
fouzulazeem98 <fouzulazeem98@gmail.com>
2024-07-13 14:13:20 +0500

    CV Site Clone

---
+++ app.js
@@ -0,0 +1,79 @@
+// let numOne = 5;
+// let numTwo = 5;
+// console.log(numOne+numTwo)
+
+// let numOne = 5;
+// let numTwo = 10;
+// console.log(numOne+numTwo)
+
+// let numOne = 5;
+// let numTwo = 5;
+// console.log("The sum of two number is: " + (numOne+numTwo))
+// console.log("The Difference of two number is: " + (numOne-numTwo))
+// console.log("The Product of two number is: " + (numOne/numTwo))
+// console.log("The Quotient of two number is: " + (numOne*numTwo))
+
+// let userName = prompt("Enter username");
+// let userPass = prompt("Enter password")
+
+// console.log(userName+userPass)
+
+
+// let numOne = 5;
+// let numTwo = 15;
+// console.log(`The sum of two number is: ${numOne+numTwo}`)
+
+
+
+
+// if (a>b & a>c) {
+//     document.write("A is greater")
+// }
+
+// else {
+//     document.write("B is greater")
+// }
+
+// if (a>b) {
+//     if (a>c) {
+//         console.log("A is greater")
+//     }
+
+
+//     else if (b>a) {
+//         if (b>c) {
+//             console.log("B is greater")
+//         }
+//     }
+
+//     else {
+//         console.log("C is greater")
+//     }
+// }
+
+
+// let numOne = prompt("Enter first number");
+// let numTwo = prompt("Enter second number");
+// let numSign = prompt("Enter sign")
+
+// if (numSign == "+") {
+//     console.log(numOne+numTwo)
+// }
+
+// else if (numSign == "-") {
+//     console.log(numOne-numTwo)
+// }
+
+// else if (numSign == "*") {
+//     console.log(numOne*numTwo)
+// }
+
+// else if (numSign == "/") {
+//     console.log(numOne/numTwo)
+// }
+
+// else if (numSign == "++" && numTwo == "") {
+//    console.log(++numOne)
+// }
+
+
